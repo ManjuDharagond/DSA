3c038bb1d0f6917ec94691aece6ec022b80b32e0
ManjuDharagond <manjudmanju.dharagond@gmail.com>
2023-12-10 15:39:59 +0530

    adding the solution for find pivot index problem

---
+++ FindPivotIndex.java
@@ -0,0 +1,18 @@
+public class FindPivotIndex {
+    class Solution {
+        public int pivotIndex(int[] nums) {
+            int totalSum = 0;
+            int leftSum = 0;
+
+            for(int num : nums)
+                totalSum += num;
+
+            for(int i =0; i<nums.length; leftSum += nums[i++])
+                if(leftSum * 2 == totalSum - nums[i])
+                    return i;
+
+            return -1;
+        }
+    }
+
+}
