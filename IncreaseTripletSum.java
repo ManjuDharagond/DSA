bfd9c6f71b2b0b948c0a3883be3551a824e730a0
ManjuDharagond <manjudmanju.dharagond@gmail.com>
2023-12-14 23:58:26 +0530

    adding the sol

---
+++ IncreaseTripletSum.java
@@ -0,0 +1,21 @@
+public class IncreaseTripletSum {
+    public class Solution {
+        public boolean increasingTriplet(int[] nums) {
+
+            int small = Integer.MAX_VALUE;
+            int mid = Integer.MAX_VALUE;
+
+
+            for(int big : nums) {
+                if(big <= small) {
+                    small = big;
+                }
+                else if(big <= mid) {
+                    mid = big;
+                }
+                else return true;
+            }
+            return false;
+        }
+    }
+}
